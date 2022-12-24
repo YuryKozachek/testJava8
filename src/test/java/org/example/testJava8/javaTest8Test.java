0836c2961d029c3e3de3f460ac2c30d196db6eb5
Yury Kozachek <show_@mail.ru>
2022-12-24 17:01:06 +0300

    add new mod

--- src/test/java/org/example/testJava8/javaTest8Test.java
+++ src/test/java/org/example/testJava8/javaTest8Test.java
@@ -7,14 +7,14 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 
 class JavaTest8Test {
     @ParameterizedTest
-    @CsvFileSource(files = "src/test/resources/javaTest8.csv")
+    @CsvFileSource(files = "src/test/resources/JavaTest8.csv")
     public void resultNumbers(int expected, int a, int b) {
         JavaTest8 javaTest8 = new JavaTest8();
         int actual = javaTest8.twoNumber(5, 3);
         Assertions.assertEquals(expected, actual);
     }
     @ParameterizedTest
-    @CsvFileSource(files = "src/test/resources/javaTest8.csv")
+    @CsvFileSource(files = "src/test/resources/JavaTest8.csv")
     public void resultNumbersReturn(int expected, int a, int b) {
         JavaTest8 javaTest8 = new JavaTest8();
         int actual = javaTest8.twoNumber(3, 5);
